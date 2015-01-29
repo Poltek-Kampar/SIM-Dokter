96de553e741fd8e5dea4fbfe7a3659cf8fdf6551
slamet-polkam <triyantoslamet88@gmail.com>
2015-01-29 14:46:54 +0700

    Upload 1
    Sistem Informasi Pasien

---
+++ src/from_login/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package from_login;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new From_Login().show();  // TODO code application logic here
+    }
+
+}
